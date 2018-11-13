54ad92a34a7f8ad3907df10bf9ced63e46361ccf
Humberto Arriaga <humberto.arriaga@dieboldnixdorf.com>
2018-11-13 16:37:37 -0600

    added flipping card logic

--- scripts.js
+++ scripts.js
@@ -1,10 +1,63 @@
+// collection of cards
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle('flip');  
+  // return if the board is locked (prevent card flipping before cards are hidden or match)
+  if (lockBoard) return;
+  
+  // return in same card click
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+  
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;  
+
+  checkForMatch();
 }
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ?  disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');    
+    resetBoard();
+  }, 1500);  
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null]
+}
+
+// Randomize cards position based on order property of each element (display: flex sorting rule)
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  })
+})();
+
+// add to click event the flip card func
 cards.forEach(card => card.addEventListener('click', flipCard));
